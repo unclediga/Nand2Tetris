f8eb8af1f3bef5a7e22155f8ed05eeee6c1e1bd7
worker <worker@firm.ru>
2024-08-26 13:21:44 +0300

    ch04 DONE

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,35 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+
+(LOOP)
+    @KBD
+    D=M
+    @FULL
+    D;JNE
+
+    @8192
+    D=A
+(KD)
+    @SCREEN
+    A=A+D
+    M=0
+    D=D-1
+    @KD
+    D;JGE
+    @LOOP
+    0;JMP
+(FULL)
+    @8192
+    D=A
+(KD1)
+    @SCREEN
+    A=A+D
+    M=-1
+    D=D-1
+    @KD1
+    D;JGE
+    @LOOP
+    0;JMP
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,4 +7,23 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+    @0
+    D=A
+    @R2
+    M=D
+(LOOP)
+    @R1
+    D=M 
+    @END
+    D;JEQ 
+    @R0  
+    D=M
+    @R2
+    M=D+M
+    @R1
+    M=M-1
+    @LOOP
+    0;JMP  
+(END)
+    @END
+    0;JMP
